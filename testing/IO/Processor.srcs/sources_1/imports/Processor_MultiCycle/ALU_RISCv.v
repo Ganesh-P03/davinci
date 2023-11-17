e8811b41e9c0d99fe777dd5877775ced2a2dfe91
Ganesh-P03 <84033688+Ganesh-P03@users.noreply.github.com>
2023-11-17 17:28:15 +0530

    Added new main decoder

--- testing/IO/Processor.srcs/sources_1/imports/Processor_MultiCycle/ALU_RISCv.v
+++ testing/IO/Processor.srcs/sources_1/imports/Processor_MultiCycle/ALU_RISCv.v
@@ -4,25 +4,26 @@ input [31:0] B;
 input [2:0] sel;
 output [31:0] ALUOut;
 output Zero,Negative,Carry,Overflow;
+// wire [31:0]sum;
 
-wire [31:0] sub, add, Anot, Bnot, mux1, mux2,and1,or1,extended;
+wire [31:0] sum, Anot, Bnot, mux1, mux2,and1,or1,extended;
 wire Cout,xor1,xnor1;
 
-add_sub  as1 ( .a(A), .b(B), .k(1'b0), .o(add), .carry(Cout));
-add_sub  as2 ( .a(A), .b(B), .k(1'b1), .o(sub), .carry(Cout));
+add_sub  as1 ( .a(A), .b(B), .k(sel[0]), .o(sum), .carry(Cout));
+// add_sub  as2 ( .a(A), .b(B), .k(1'b1), .o(sub), .carry(Cout));
 NOT_32bit n1 ( .in(A), .out(Anot));
-MUX2x1_32bit m1( .a(Anot), .b(A), .s(sel[0]), .y(mux1));
+MUX2x1_32bit m1( .a(A), .b(Anot), .s(sel[0]), .y(mux1));
 NOT_32bit n2 ( .in(B), .out(Bnot));
-MUX2x1_32bit m2 ( .a(Bnot), .b(B), .s(sel[0]), .y(mux2));
+MUX2x1_32bit m2 ( .a(B), .b(Bnot), .s(sel[0]), .y(mux2));
 bitwiseAND ba ( .a(mux1), .b(mux2), .out(and1));
 NOT_32bit n3 ( .in(and1), .out(or1));
-XOR_1bit_2inp x1 ( .i1(A[31]), .i2(add[31]), .o(xor1));
+XOR_1bit_2inp x1 ( .i1(A[31]), .i2(sum[31]), .o(xor1));
 XNOR_1bit_3inp x2 ( .i1(A[31]), .i2(B[31]), .i3(sel[0]), .o(xnor1));
 AND_1bit_3inp a1 ( .i1( ~ sel[1]), .i2(xor1), .i3(xnor1), .o(Overflow));
 AND_1bit_2inp a2 ( .i1( ~ sel[1]), .i2(Cout), .o(Carry));
-XOR_1bit_2inp x3 ( .i1(add[31]), .i2(Overflow), .o(xor2));
+XOR_1bit_2inp x3 ( .i1(sum[31]), .i2(Overflow), .o(xor2));
 zero_extender ze1 ( .inp(xor2), .out(extended));
-MUX8x1_32bit m3 (.i0(add),.i1(sub), .i2(or1), .i3(and1), .i4(extended), .i5(32'b0), .i6(32'b0), .i7(32'b0), .sel(sel), .o(ALUOut));
+MUX8x1_32bit m3 (.i0(sum),.i1(sum), .i2(and1), .i3(or1), .i4(32'b0), .i5(extended), .i6(32'b0), .i7(32'b0), .sel(sel), .o(ALUOut));
 
 //zero and negative flags
 assign Zero = (ALUOut == 32'b0);
--- testing/IO/Processor.srcs/sources_1/imports/Processor_MultiCycle/MUX4x1_32bit.v
+++ testing/IO/Processor.srcs/sources_1/imports/Processor_MultiCycle/MUX4x1_32bit.v
@@ -7,12 +7,15 @@ module MUX4x1_32bit (
   output [31:0] o
 );
 
-wire [31:0] m0, m1, m2;
+// wire [31:0] m0, m1, m2;
 
-MUX2x1_32bit mux0 (.a(i0), .b(i1), .s(sel[0]), .y(m0));
-MUX2x1_32bit mux1 (.a(i2), .b(i3), .s(sel[0]), .y(m1));
-MUX2x1_32bit mux2 (.a(m0), .b(m1), .s(sel[1]), .y(m2));
+// MUX2x1_32bit mux0 (.a(i0), .b(i1), .s(sel[0]), .y(m0));
+// MUX2x1_32bit mux1 (.a(i2), .b(i3), .s(sel[0]), .y(m1));
+// MUX2x1_32bit mux2 (.a(m0), .b(m1), .s(sel[1]), .y(m2));
 
-assign o = m2;
+assign o = (sel==2'b00)?i0:
+           (sel==2'b01)?i1:
+           (sel==2'b10)?i2:
+           (sel==2'b11)?i3:32'b0;
 
 endmodule
